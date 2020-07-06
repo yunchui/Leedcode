0072a5c031da611039e8aa787d2516b26b9ddb99
yunchui <413062349@qq.com>
2020-07-06 11:12:32 +0800

    add 367 326

---
+++ 326.3-的幂.py
@@ -0,0 +1,50 @@
+#
+# @lc app=leetcode.cn id=326 lang=python3
+#
+# [326] 3的幂
+#
+# https://leetcode-cn.com/problems/power-of-three/description/
+#
+# algorithms
+# Easy (46.88%)
+# Likes:    112
+# Dislikes: 0
+# Total Accepted:    49.9K
+# Total Submissions: 106.4K
+# Testcase Example:  '27'
+#
+# 给定一个整数，写一个函数来判断它是否是 3 的幂次方。
+# 
+# 示例 1:
+# 
+# 输入: 27
+# 输出: true
+# 
+# 
+# 示例 2:
+# 
+# 输入: 0
+# 输出: false
+# 
+# 示例 3:
+# 
+# 输入: 9
+# 输出: true
+# 
+# 示例 4:
+# 
+# 输入: 45
+# 输出: false
+# 
+# 进阶：
+# 你能不使用循环或者递归来完成本题吗？
+# 
+#
+
+# @lc code=start
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        return (n > 0)and(1162261467 %n == 0)
+        
+# @lc code=end
+
