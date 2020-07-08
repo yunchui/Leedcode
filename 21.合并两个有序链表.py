f6ea42ae53726ecc55d151523b1dc20683da125c
yunchui <413062349@qq.com>
2020-07-08 14:37:13 +0800

    add 21

---
+++ 21.合并两个有序链表.py
@@ -0,0 +1,64 @@
+#
+# @lc app=leetcode.cn id=21 lang=python3
+#
+# [21] 合并两个有序链表
+#
+# https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+#
+# algorithms
+# Easy (62.45%)
+# Likes:    1142
+# Dislikes: 0
+# Total Accepted:    307.3K
+# Total Submissions: 486.2K
+# Testcase Example:  '[1,2,4]\n[1,3,4]'
+#
+# 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+# 
+# 
+# 
+# 示例：
+# 
+# 输入：1->2->4, 1->3->4
+# 输出：1->1->2->3->4->4
+# 
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1 or not l2:
+            return l1 or l2
+        new_l = ListNode(0)
+        cur = new_l
+        cur1 = l1
+        cur2 = l2
+        while cur1 and cur2:
+            if cur1.val <= cur2.val:
+                cur.next = cur1
+                cur1 = cur1.next
+            else:
+                cur.next = cur2
+                cur2 = cur2.next
+            cur = cur.next
+        if cur1:
+            cur.next = cur1
+        if cur2:
+            cur.next = cur2
+        return new_l.next
+        
+                
+
+
+
+
+            
+        
+# @lc code=end
+
