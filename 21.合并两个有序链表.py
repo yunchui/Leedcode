7abde52e0dea8be556ea53a545b0c8a9422ab4ab
yunchui <413062349@qq.com>
2020-07-08 18:36:19 +0800

    update 26

--- 21.合并两个有序链表.py
+++ 21.合并两个有序链表.py
@@ -33,6 +33,19 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        if not l1 or not l2:
+            return l1 or l2
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next,l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1,l2.next)
+            return l2           
+'''
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+ 
         if not l1 or not l2:
             return l1 or l2
         new_l = ListNode(0)
@@ -53,9 +66,7 @@ class Solution:
             cur.next = cur2
         return new_l.next
         
-                
-
-
+'''
 
 
             
