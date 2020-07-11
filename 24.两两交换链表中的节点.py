724bcdfc39f61a6f1ed59b35b4ef5140a0e02335
yunchui <413062349@qq.com>
2020-07-11 20:30:25 +0800

    add 24

---
+++ 24.两两交换链表中的节点.py
@@ -0,0 +1,51 @@
+#
+# @lc app=leetcode.cn id=24 lang=python3
+#
+# [24] 两两交换链表中的节点
+#
+# https://leetcode-cn.com/problems/swap-nodes-in-pairs/description/
+#
+# algorithms
+# Medium (65.63%)
+# Likes:    545
+# Dislikes: 0
+# Total Accepted:    122.6K
+# Total Submissions: 185.4K
+# Testcase Example:  '[1,2,3,4]'
+#
+# 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+# 
+# 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+# 
+# 
+# 
+# 示例:
+# 
+# 给定 1->2->3->4, 你应该返回 2->1->4->3.
+# 
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if head == None or head.next == None:
+            return head
+        p = head
+        q = head.next
+        p.next = self.swapPairs(q.next)
+        
+        q.next = p
+        return q
+
+
+
+
+# @lc code=end
+
