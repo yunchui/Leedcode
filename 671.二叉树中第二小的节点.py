e75d314de59b29aa98adf810a2c7aa66d5a15930
yunchui <413062349@qq.com>
2020-07-24 14:18:47 +0800

    add 671

---
+++ 671.二叉树中第二小的节点.py
@@ -0,0 +1,71 @@
+#
+# @lc app=leetcode.cn id=671 lang=python3
+#
+# [671] 二叉树中第二小的节点
+#
+# https://leetcode-cn.com/problems/second-minimum-node-in-a-binary-tree/description/
+#
+# algorithms
+# Easy (46.48%)
+# Likes:    89
+# Dislikes: 0
+# Total Accepted:    13.8K
+# Total Submissions: 29.5K
+# Testcase Example:  '[2,2,5,null,null,5,7]'
+#
+# 给定一个非空特殊的二叉树，每个节点都是正数，并且每个节点的子节点数量只能为 2 或
+# 0。如果一个节点有两个子节点的话，那么该节点的值等于两个子节点中较小的一个。
+# 
+# 给出这样的一个二叉树，你需要输出所有节点中的第二小的值。如果第二小的值不存在的话，输出 -1 。
+# 
+# 示例 1:
+# 
+# 输入: 
+# ⁠   2
+# ⁠  / \
+# ⁠ 2   5
+# ⁠    / \
+# ⁠   5   7
+# 
+# 输出: 5
+# 说明: 最小的值是 2 ，第二小的值是 5 。
+# 
+# 
+# 示例 2:
+# 
+# 输入: 
+# ⁠   2
+# ⁠  / \
+# ⁠ 2   2
+# 
+# 输出: -1
+# 说明: 最小的值是 2, 但是不存在第二小的值。
+# 
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def findSecondMinimumValue(self, root: TreeNode) -> int:
+        judge = root.val
+        def find(root):
+
+            if not root:
+                return float('inf')
+            
+            if root.val == judge:
+                return min(find(root.left),find(root.right))
+            return min(root.val,find(root.left),find(root.right))
+        second = find(root)
+        return second if second !=float('inf') else -1
+            
+        
+# @lc code=end
+
