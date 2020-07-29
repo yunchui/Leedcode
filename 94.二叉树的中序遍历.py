edb8a2feaf1063893c3114b665e761fd333d1fa6
yunchui <413062349@qq.com>
2020-07-29 09:34:42 +0800

    add 94

---
+++ 94.二叉树的中序遍历.py
@@ -0,0 +1,58 @@
+#
+# @lc app=leetcode.cn id=94 lang=python3
+#
+# [94] 二叉树的中序遍历
+#
+# https://leetcode-cn.com/problems/binary-tree-inorder-traversal/description/
+#
+# algorithms
+# Medium (71.46%)
+# Likes:    595
+# Dislikes: 0
+# Total Accepted:    200.9K
+# Total Submissions: 278.2K
+# Testcase Example:  '[1,null,2,3]'
+#
+# 给定一个二叉树，返回它的中序 遍历。
+# 
+# 示例:
+# 
+# 输入: [1,null,2,3]
+# ⁠  1
+# ⁠   \
+# ⁠    2
+# ⁠   /
+# ⁠  3
+# 
+# 输出: [1,3,2]
+# 
+# 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        ret = []
+        stack = []
+        if not root:
+            return ret
+        
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            node = stack.pop()
+            ret.append(node.val)
+            root = node.right
+        return ret
+
+# @lc code=end
+
