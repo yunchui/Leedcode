0befa74daedfbfb899b9d4c7a126cd580e41be19
yunchui <413062349@qq.com>
2020-08-01 09:10:45 +0800

    add 230

---
+++ 230.二叉搜索树中第k小的元素.py
@@ -0,0 +1,73 @@
+#
+# @lc app=leetcode.cn id=230 lang=python3
+#
+# [230] 二叉搜索树中第K小的元素
+#
+# https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/description/
+#
+# algorithms
+# Medium (70.39%)
+# Likes:    250
+# Dislikes: 0
+# Total Accepted:    62.2K
+# Total Submissions: 87.2K
+# Testcase Example:  '[3,1,4,null,2]\n1'
+#
+# 给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
+# 
+# 说明：
+# 你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。
+# 
+# 示例 1:
+# 
+# 输入: root = [3,1,4,null,2], k = 1
+# ⁠  3
+# ⁠ / \
+# ⁠1   4
+# ⁠ \
+# 2
+# 输出: 1
+# 
+# 示例 2:
+# 
+# 输入: root = [5,3,6,2,4,null,null,1], k = 3
+# ⁠      5
+# ⁠     / \
+# ⁠    3   6
+# ⁠   / \
+# ⁠  2   4
+# ⁠ /
+# ⁠1
+# 输出: 3
+# 
+# 进阶：
+# 如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化 kthSmallest 函数？
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        self.cnt = 0
+        self.val = 0
+        def inOrder(node,k):
+            if node == None:
+                return 
+            inOrder(node.left,k)
+            self.cnt+=1
+            if self.cnt == k:
+                self.val = node.val
+                return
+            inOrder(node.right,k)
+        inOrder(root,k)
+        return self.val
+            
+        
+# @lc code=end
+
