fead7d201affac79d5b4b3c5c6555f133d3f0444
yunchui <413062349@qq.com>
2020-08-14 11:06:43 +0800

    add 232

---
+++ 232.用栈实现队列.py
@@ -0,0 +1,108 @@
+#
+# @lc app=leetcode.cn id=232 lang=python3
+#
+# [232] 用栈实现队列
+#
+# https://leetcode-cn.com/problems/implement-queue-using-stacks/description/
+#
+# algorithms
+# Easy (64.03%)
+# Likes:    211
+# Dislikes: 0
+# Total Accepted:    57.5K
+# Total Submissions: 88.9K
+# Testcase Example:  '["MyQueue","push","push","peek","pop","empty"]\n[[],[1],[2],[],[],[]]'
+#
+# 使用栈实现队列的下列操作：
+# 
+# 
+# push(x) -- 将一个元素放入队列的尾部。
+# pop() -- 从队列首部移除元素。
+# peek() -- 返回队列首部的元素。
+# empty() -- 返回队列是否为空。
+# 
+# 
+# 
+# 
+# 示例:
+# 
+# MyQueue queue = new MyQueue();
+# 
+# queue.push(1);
+# queue.push(2);  
+# queue.peek();  // 返回 1
+# queue.pop();   // 返回 1
+# queue.empty(); // 返回 false
+# 
+# 
+# 
+# 说明:
+# 
+# 
+# 你只能使用标准的栈操作 -- 也就是只有 push to top, peek/pop from top, size, 和 is empty
+# 操作是合法的。
+# 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+# 假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）。
+# 
+# 
+#
+
+# @lc code=start
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack1 = []
+        self.stack2 = []
+
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.stack1.append(x)
+
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        elem = self.stack2.pop()
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+        return elem
+
+
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        elem = self.stack2[len(self.stack2)-1]
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+        return elem
+
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return not self.stack1
+
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
+# @lc code=end
+
