96741dba0192811265a8a2511585ce384283c784
yunchui <413062349@qq.com>
2020-08-22 13:47:50 +0800

    add 594

---
+++ 594.最长和谐子序列.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode.cn id=594 lang=python3
+#
+# [594] 最长和谐子序列
+#
+# https://leetcode-cn.com/problems/longest-harmonious-subsequence/description/
+#
+# algorithms
+# Easy (46.92%)
+# Likes:    117
+# Dislikes: 0
+# Total Accepted:    14.5K
+# Total Submissions: 30.2K
+# Testcase Example:  '[1,3,2,2,5,2,3,7]'
+#
+# 和谐数组是指一个数组里元素的最大值和最小值之间的差别正好是1。
+# 
+# 现在，给定一个整数数组，你需要在所有可能的子序列中找到最长的和谐子序列的长度。
+# 
+# 示例 1:
+# 
+# 
+# 输入: [1,3,2,2,5,2,3,7]
+# 输出: 5
+# 原因: 最长的和谐数组是：[3,2,2,2,3].
+# 
+# 
+# 说明: 输入的数组长度最大不超过20,000.
+# 
+#
+
+# @lc code=start
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        dic = {}
+        res = 0
+        for num in nums:
+            if num not in dic:
+                dic[num] = 0
+            dic[num] += 1
+            m = dic[num] + dic[num-1] if num-1 in dic else 0
+            n = dic[num] + dic[num+1] if num+1 in dic else 0
+            res = max(m,n,res)
+        return res
+
+
+        
+# @lc code=end
+
